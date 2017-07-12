f1c9e63cea5698a1557c22eb38927670a26fce68
John C Barstow <jbowtie@amathaine.com>
2017-07-12 20:43:23 +1200

    Move basic OpenType tests out of gutenex

--- test/font_test.exs
+++ test/font_test.exs
@@ -2,20 +2,6 @@ defmodule GutenexFontTest do
   use ExUnit.Case
   alias OpenType, as: TrueType
   alias OpenTypeFont
-  alias OpenType.Layout
-
-  test "parse Truetype font metrics" do
-    ttf = TrueType.new
-          |> TrueType.parse("./test/support/fonts/NotoSansCJKjp-Bold.otf")
-    assert 733 == ttf.capHeight
-    assert -120 == ttf.descent
-    assert 880 == ttf.ascent
-    assert [-1013.0, -1046.0, 2926.0, 1806.0] == ttf.bbox
-    assert 0 == ttf.italicAngle
-    assert 1000 == ttf.unitsPerEm
-    assert 1000 == ttf.defaultWidth
-    assert 700 == ttf.usWeightClass
-  end
 
   test "register parsed font" do
     {:ok, pid} = Gutenex.start_link
@@ -31,90 +17,6 @@ defmodule GutenexFontTest do
     # now font exists
     ctx = Gutenex.context(pid)
     assert Map.has_key?(ctx.fonts, ttf.name)
-  end
-
-  test "Basic CMAP support" do
-    ttf = TrueType.new
-          |> TrueType.parse("./test/support/fonts/NotoSans-Bold.ttf")
-    {glyphs, _} = TrueType.layout_text(ttf, "ABC")
-    assert glyphs == [36, 37, 38]
-  end
-
-  test "Apply OpenType substitutions (GSUB 4) - exercise ligature" do
-    ttf = TrueType.new
-          |> TrueType.parse("./test/support/fonts/NotoSans-Bold.ttf")
-    {glyphs, _} = TrueType.layout_text(ttf, "ffl", ["liga"])
-    assert glyphs == [603]
-  end
-
-  test "Apply OpenType substitutions (GSUB 6) - exercise chained" do
-    ttf = TrueType.new
-          |> TrueType.parse("./test/support/fonts/SourceSansPro-Regular.otf")
-    {glyphs, _} = TrueType.layout_text(ttf, "1/2", ["liga", "frac"])
-    assert glyphs == [1617, 1726, 1604]
-  end
-
-  test "Apply mark-to-base positioning" do
-    ttf = TrueType.new
-          |> TrueType.parse("./test/support/fonts/NotoSans-Bold.ttf")
-    {glyphs, pos} = TrueType.layout_text(ttf, "bi\u0300g", ["ccmp", "mark"])
-    # ccmp will replace i with dotless variant
-    assert glyphs == [69, 243, 608, 74]
-
-    # mark will position accent over the i
-    xpos = pos |> Enum.map(fn {_,x,_,_,_} -> x end)
-    xadv = pos |> Enum.map(fn {_,_,_,x,_} -> x end)
-    # mark has an xOffset
-    assert xpos == [0, 0, 317, 0]
-    # mark has zero width
-    assert xadv == [1296, 625, 0, 1296]
-  end
-
-  test "Exercise GSUB 2 - one-to-many substitution" do
-    ttf = TrueType.new
-          |> TrueType.parse("./test/support/fonts/NotoNastaliqUrdu-Regular.ttf")
-    # include a following character to ensure returned glyphs are flattened properly
-    # Qaf, Alef
-    {glyphs, _} = TrueType.layout_text(ttf, "\u0642\u0627", ["ccmp"])
-    # decompose QAF into QAFX and TWO_DOTS_ABOVE
-    assert glyphs == [858, 16, 889]
-  end
-
-  test "Test positional substitutions" do
-    ttf = TrueType.new
-          |> TrueType.parse("./test/support/fonts/NotoNastaliqUrdu-Regular.ttf")
-
-    # no substitutions
-    {glyphs, _} = TrueType.layout_text(ttf, "\u0644", [])
-    assert glyphs == [881]
-
-    # enable isolation
-    {glyphs, _} = TrueType.layout_text(ttf, "\u0644", ["isol"])
-    assert glyphs == [248]
-
-    # standard shaping
-    # init, media, fina, isol
-    {glyphs, _} = TrueType.layout_text(ttf, "\u0642\u0644\u0627\u06A9")
-    assert glyphs == [247, 279, 268, 16, 304]
-
-    {glyphs, _} = TrueType.layout_text(ttf, "\u0644\u0627\u06A9\u0642")
-    # init, fina, init, fina
-    assert glyphs == [16, 391, 422, 279, 267]
-
-    {glyphs, _} = TrueType.layout_text(ttf, "\u0627\u06A9\u0642\u0644")
-    # isol, init, medi, fina
-    assert glyphs ==  [16, 460, 249, 717, 227]
-  end
-
-  test "Urdu cursive" do
-    ttf = TrueType.new
-          |> TrueType.parse("./test/support/fonts/NotoNastaliqUrdu-Regular.ttf")
-
-    s = Layout.detect_script("\u062D\u062D\u062D\u062D\u062D\u062D\u0628")
-    assert s == "arab"
-
-    {glyphs, _pos} = TrueType.layout_text(ttf, "\u062D\u062D\u062D\u062D\u062D\u062D\u0628")
-    assert glyphs ==  [230, 591, 591, 591, 591, 591, 18, 523]
   end
 
   # turn OpenType features on and off in an integration test
